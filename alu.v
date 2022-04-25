f0f7a298f87bcbeaa59207237ef0607f4594defd
John <john.nycjets@gmail.com>
2022-04-25 19:20:30 -0400

    more changes

--- alu.v
+++ alu.v
@@ -10,7 +10,15 @@ assign ALU_Out = Result;
 always @(*) 
 begin 
 case(ALU_Code)
-    3'b000: Result = A + B; //ADD 
+    3'b000: Result = A + B; //ADD
+    3'b001: Result = A - B; //SUB
+    3'b010: Result = A>>1; //LSL
+    3'b011: Result = A & B; //AND
+    3'b101: Result = A | B; //A or B
+    3'b110: Result = ~(A); //Not
+    3'b111: Result = A^B; //Exclusive OR
+    //probably should add mult div etc 
+
 endcase
 
 
