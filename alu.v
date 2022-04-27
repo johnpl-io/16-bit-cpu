325cfe5bc6ac0c5b8cd1d51cdedcb7c2f55f9fcc
Your Name <you@example.com>
2022-04-26 20:26:14 -0400

    fixes

--- alu.v
+++ alu.v
@@ -2,9 +2,9 @@ module alu(
     input [7:0] A, 
     input [7:0] B,
     input [2:0] ALU_Code,
-    output [7:0] ALU_Out, //,
-   output Carry,
-   output isZero);
+    output [7:0] ALU_Out,
+    output Carry,
+    output isZero);
 
 reg [7:0] Result;
 assign ALU_Out = Result;
@@ -17,13 +17,13 @@ always @(*)
 begin 
 case(ALU_Code)
     3'b000: {carry,Result} = A + B; //ADD
-    3'b001: Result = A - B; //SUB
-    3'b010: Result = A>>1; //LSL
-    3'b011: Result = A<<1; //LSR
-    3'b100: Result = A & B; //AND
-    3'b101: Result = A | B; //A or B
-    3'b110: Result = ~(A); //Not
-    3'b111: Result = A^B; //Exclusive OR
+    3'b001: Result = A - B;         //SUB
+    3'b010: Result = A>>1;          //LSL
+    3'b011: Result = A<<1;          //LSR
+    3'b100: Result = A & B;         //AND
+    3'b101: Result = A | B;         //A or B
+    3'b110: Result = ~(A);          //Not
+    3'b111: Result = A^B;           //Exclusive OR
     //probably should add mult div etc 
     default: Result = A+B;
 endcase
