01f4488d6719730fc8b5304a7700db17fe9457c4
john.nycjets@gmail.com <john.nycjets@gmail.com>
2022-05-03 19:42:02 -0400

    test

--- controller.v
+++ controller.v
@@ -4,7 +4,7 @@ wire [15:0] instruction;
 reg clk;
 wire [15:0] reg1, reg2, wreg;
 wire [3:0] ALU_Code;
-
+//decoding 
 assign opcode = instruction[15:9];
 assign wreg = instruction[8:6];
 assign reg1 = instruction[5:3];
