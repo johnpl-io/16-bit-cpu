55eb3343c13931f0c5564c794bc59225aaca33c1
john.nycjets@gmail.com <john.nycjets@gmail.com>
2022-05-12 20:02:57 -0400

    test

--- cpu.v
+++ cpu.v
@@ -15,7 +15,7 @@ wire [15:0] res;
 wire Carry;
 wire isZero;
 
-wire [2:0] reg1, reg2, wreg;
+wire [2:0] rs, rt, rd;
 wire [2:0] ALU_Code;
 wire [2:0] opcode;
 wire [3:0] func;
@@ -29,15 +29,15 @@ wire [15:0] alumuxout;
 
 //decoding 
 assign opcode = instruction[15:13];
-assign wreg = instruction[12:10];
-assign reg1 = instruction[9:7];
-assign reg2 = instruction[6:4];
+assign rs = instruction[12:10];
+assign rt = instruction[9:7];
+assign rd = instruction[6:4];
 assign func = instruction[3:0];
 assign immediate = instruction[6:0];
 
 
 alu alu_test(.A(read1), .B(alumuxout), .ALU_Code(ALU_Code), .ALU_Out(res), .Carry(Carry), .isZero(isZero));
-regfile reg_test(.clk(clk), .write_en(regwrite), .rega(reg1), .regb(reg2), .wreg(wreg), .writedata(res), .read1(read1), .read2(read2));
+regfile reg_test(.clk(clk), .write_en(regwrite), .rega(rs), .regb(rt), .wreg(rd), .writedata(res), .read1(read1), .read2(read2));
 imem imem_test(.pc(pc), .instruction(instruction));
 alumux alumux_test(.immediate(immediate), .read2(read2), .aluop(aluop), .alumuxout(alumuxout));
 control control_test(.opcode(opcode), .jump(jump), .branch(branch), .memwrite(memwrite), .regwrite(regwrite), .aluop(aluop));
@@ -54,8 +54,10 @@ always @(posedge clk)
 begin 
      $display("pc : %b instruction : %b", pc, instruction);
     pc = pc + 1;
-   
-    if(pc == 3) 
+   if(isZero == 0)
+   #10
+    $display("yo");
+    if(pc == 3)
     $finish;
     
 end
